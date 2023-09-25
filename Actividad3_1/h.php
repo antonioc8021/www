12fb588c75c06593a5473ce9b9976924940e2b25
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-09-25 23:42:29 +0200

    piramides

--- Actividad3_1/h.php
+++ Actividad3_1/h.php
@@ -6,6 +6,20 @@ for ($i = 1; $i <= 10; $i++) {
             echo " ";
         }
     }
-    echo "\n";
+    echo "<br/>";
 }
+echo "Y otro que muestre: <br/>";
+
+for ($i = 10; $i >= 1; $i--) {
+    // Imprime números desde $i hasta 1 en cada fila
+    for ($j = $i; $j >= 1; $j--) {
+        echo $j;
+        if ($j > 1) {
+            echo " ";
+        }
+    }
+    echo "<br/>";
+}
+
+
 ?>
