6c6d97216d71ca01c65ee0626c686d34e34e3bba
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-09-28 13:49:29 +0200

    insert teacher´s code

--- Actividad3_3/e.php
+++ Actividad3_3/e.php
@@ -1,4 +1,9 @@
 <?php
+// consultar esta forma de hacerlo: (same that teacher);
+// https://www.php.net/manual/es/function.checkdate
+
+
+// my option it´s not the best;
 function validaFecha($mes, $dia, $ano)
 {
     if (checkdate($mes, $dia, $ano)) {
@@ -9,5 +14,21 @@ function validaFecha($mes, $dia, $ano)
 
 }
 
-echo (validaFecha(5, 24, 2002))
-    ?>
+// by the teacher:
+function comprobarFecha($fecha)
+{
+    $dia = (int) substr($fecha, 0, 2);
+    $mes = (int) substr($fecha, 3, 2);
+    $ano = (int) substr($fecha, 6, 4);
+
+    return checkdate($mes, $dia, $ano);
+}
+
+echo ("<br>La fecha es: ");
+$fecha = "30-05 -2020";
+echo comprobarFecha($fecha) ? "correcta" : "incorrecta";
+
+echo ("<br>La fecha es: ");
+$fecha = "30-02-2020";
+echo comprobarFecha($fecha) ? "correcta" : "incorrecta";
+?>
