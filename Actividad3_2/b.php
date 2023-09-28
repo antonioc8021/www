947f927d79dc5924cb7c6bd8c467ff539791e690
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-09-28 13:04:06 +0200

    correct small changes

--- Actividad3_2/b.php
+++ Actividad3_2/b.php
@@ -7,23 +7,13 @@
 </head>
 
 <body>
-    <h1>Fecha Actual en Castellano</h1>
-
     <?php
-    // Días de la semana en castellano
     $diasSemana = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado');
-
-    // Meses en castellano
     $meses = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
-
-    // Obtener la fecha actual
     $fechaActual = date('d-m-Y');
     $numeroDiaSemana = date('w', strtotime($fechaActual));
     $numeroMes = date('n', strtotime($fechaActual));
-
-    // Crear la cadena de fecha en castellano
     $fechaCastellano = $diasSemana[$numeroDiaSemana] . ', ' . date('j', strtotime($fechaActual)) . ' de ' . $meses[$numeroMes - 1] . ' de ' . date('Y', strtotime($fechaActual));
-
     echo '<p>' . $fechaCastellano . '</p>';
     ?>
 
