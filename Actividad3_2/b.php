950e1ac97d5937b54eed280de20289925953a3a4
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-09-28 13:01:43 +0200

    Actividad3_2/b.php is done

--- Actividad3_2/b.php
+++ Actividad3_2/b.php
@@ -1,5 +1,32 @@
-<?php
-echo "La hora es: " . date("h:i:sa");
-// como coño tengo que hacer esto no lo entiendo de verdad
+<!DOCTYPE html>
+<html>
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <title>Fecha Actual en Castellano</title>
+</head>
+
+<body>
+    <h1>Fecha Actual en Castellano</h1>
+
+    <?php
+    // Días de la semana en castellano
+    $diasSemana = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado');
+
+    // Meses en castellano
+    $meses = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
+
+    // Obtener la fecha actual
+    $fechaActual = date('d-m-Y');
+    $numeroDiaSemana = date('w', strtotime($fechaActual));
+    $numeroMes = date('n', strtotime($fechaActual));
+
+    // Crear la cadena de fecha en castellano
+    $fechaCastellano = $diasSemana[$numeroDiaSemana] . ', ' . date('j', strtotime($fechaActual)) . ' de ' . $meses[$numeroMes - 1] . ' de ' . date('Y', strtotime($fechaActual));
+
+    echo '<p>' . $fechaCastellano . '</p>';
+    ?>
+
+</body>
+
+</html>
