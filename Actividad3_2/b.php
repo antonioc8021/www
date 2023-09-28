aa312f06fdff28d36655a04578cb32ce3bbfebc9
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-09-28 13:08:04 +0200

    made by the teacher

--- Actividad3_2/b.php
+++ Actividad3_2/b.php
@@ -2,21 +2,77 @@
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <title>Fecha Actual en Castellano</title>
+    <title>Fecha en castellano</title>
 </head>
 
 <body>
     <?php
-    $diasSemana = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado');
-    $meses = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
-    $fechaActual = date('d-m-Y');
-    $numeroDiaSemana = date('w', strtotime($fechaActual));
-    $numeroMes = date('n', strtotime($fechaActual));
-    $fechaCastellano = $diasSemana[$numeroDiaSemana] . ', ' . date('j', strtotime($fechaActual)) . ' de ' . $meses[$numeroMes - 1] . ' de ' . date('Y', strtotime($fechaActual));
-    echo '<p>' . $fechaCastellano . '</p>';
+    date_default_timezone_set('Europe/Madrid');
+    $numero_mes = date("m");
+    $numero_dia_semana = date("N");
+    switch ($numero_mes) {
+        case 1:
+            $mes = "Enero";
+            break;
+        case 2:
+            $mes = "Febrero";
+            break;
+        case 3:
+            $mes = "Marzo";
+            break;
+        case 4:
+            $mes = "Abril";
+            break;
+        case 5:
+            $mes = "Mayo";
+            break;
+        case 6:
+            $mes = "Junio";
+            break;
+        case 7:
+            $mes = "Julio";
+            break;
+        case 8:
+            $mes = "Agosto";
+            break;
+        case 9:
+            $mes = "Septiembre";
+            break;
+        case 10:
+            $mes = "Octubre";
+            break;
+        case 11:
+            $mes = "Noviembre";
+            break;
+        case 12:
+            $mes = "Diciembre";
+            break;
+    }
+    switch ($numero_dia_semana) {
+        case 1:
+            $dia_semana = "Lunes";
+            break;
+        case 2:
+            $dia_semana = "Martes";
+            break;
+        case 3:
+            $dia_semana = "Miércoles";
+            break;
+        case 4:
+            $dia_semana = "Jueves";
+            break;
+        case 5:
+            $dia_semana = "Viernes";
+            break;
+        case 6:
+            $dia_semana = "Sábado";
+            break;
+        case 7:
+            $dia_semana = "Domingo";
+            break;
+    }
+    print "Hoy es: " . $dia_semana . ", " . date("j") . " de " . $mes . " de " . date("Y");
     ?>
-
 </body>
 
 </html>
