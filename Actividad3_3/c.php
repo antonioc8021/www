86ff74e5e4095897d052d848e241c5e4515895fc
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-09-28 12:36:34 +0200

    c.php is finished

---
+++ Actividad3_3/c.php
@@ -0,0 +1,19 @@
+<?php
+function factorial($number)
+{
+    if (is_numeric($number) == false) {
+        return (-1);
+    } else {
+
+        $res = 1;
+        for ($i = $number; $i > 0; $i--) {
+            $res *= $i;
+        }
+        return $res;
+
+    }
+
+}
+
+echo (factorial(50));
+?>
