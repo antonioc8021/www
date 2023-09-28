4365afa687fe7f9ba54bb1bb283dc5194e75dacd
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-09-28 13:35:10 +0200

    f.php is complete, make an argument optional

---
+++ Actividad3_3/f.php
@@ -0,0 +1,14 @@
+<?php
+
+
+//if you asigment any value to the argument it´s optional to put any new value.
+function potencia($num, $exponente = 2)
+{
+    return pow($num, $exponente);
+}
+
+
+echo (potencia(2, 3) . '<br>');
+echo potencia(40)
+
+    ?>
