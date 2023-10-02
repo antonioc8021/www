03a5fe8bb7bf3543dab3b16d024619a93e799d6e
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-02 11:37:35 +0200

    exercises 7.php not finished

--- Actividad3_3/f.php
+++ Actividad3_3/f.php
@@ -1,7 +1,7 @@
 <?php
 
 
-//if you asigment any value to the argument it´s optional to put any new value.
+//if you asigment any value to the argument it´s optional to put any new value. 
 function potencia($num, $exponente = 2)
 {
     return pow($num, $exponente);
