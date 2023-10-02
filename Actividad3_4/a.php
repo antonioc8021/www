8284cab0c0e2460443959ac663d4fe9c61da149d
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-02 13:36:06 +0200

    the teacher mader better than me this exercise

--- Actividad3_4/a.php
+++ Actividad3_4/a.php
@@ -1,7 +1,17 @@
 <?php
-function generarArray()
+
+$cadena1 = [];
+$cadena2 = [];
+
+function generarArray(&$cadena1, &$cadena2)
 {
+    for ($i = 0; $i < 20; $i++) {
+        $cadena1[$i] = rand(0, 100);
+        $cadena2[$i] = rand(0, 100);
+    }
 
 }
 
+generarArray($cadena1, $cadena2);
+echo $cadena1 . $cadena2;
 ?>
