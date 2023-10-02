73e43aedc90450b7b23686643c22cb1b0018de2b
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-02 19:21:32 +0200

    Ejer1.php finished

--- Tarea1/Ejer1.php
+++ Tarea1/Ejer1.php
@@ -9,9 +9,29 @@
 
 <body>
     <?php
-    define("pi", pi());
-    echo pi;
+    // define is for declare any constant, you can use it for put inside any array or anything;
+    define("PI", PI());
 
+    function area($radio)
+    {
+        return PI * pow($radio, 2);
+    }
+
+    function volumen($radio)
+    {
+        return 4 / 3 * PI * pow($radio, 3);
+    }
+
+    function perimetro($radio)
+    {
+        return 2 * PI * $radio;
+    }
+
+    define("RADIO", 10);
+    echo "El radio utilizado en todos los ejemplos es: " . RADIO . "<br>";
+    echo "El área es: " . area(RADIO) . "<br>";
+    echo "El volumen es: " . volumen(RADIO) . "<br>";
+    echo "El perímetro es: " . perimetro(RADIO) . "<br>";
     ?>
 </body>
 
