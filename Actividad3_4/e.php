0d6716d00f7411b51d33d4f0c062d9bd9b3281d5
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-03 13:00:18 +0200

    create e.php, have an array with countries

---
+++ Actividad3_4/e.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $paises = [
+        "España" => "Madrid",
+        "Alemania" => "Berlin",
+        "Italia" => "Roma",
+        "Francia" => "Paris",
+        "Portugal" => "Lisboa",
+        "Suecia" => "Estocolmo"
+    ];
+
+    print_r($paises);
+
+    ?>
+</body>
+
+</html>
