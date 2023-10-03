8f2eb9e0ce3b0e429440333d41b23f234ebe8324
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-03 12:45:41 +0200

    rescue method from the control version

--- Actividad3_4/d.php
+++ Actividad3_4/d.php
@@ -1,5 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>d</title>
+</head>
 
+<body>
+    <?php
+    function fecha()
+    {
+        $diasSemana = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado');
+        $meses = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
+        $fechaActual = date('d-m-Y');
+        $numeroDiaSemana = date('w', strtotime($fechaActual));
+        $numeroMes = date('n', strtotime($fechaActual));
+        $fechaCastellano = $diasSemana[$numeroDiaSemana] . ', ' . date('j', strtotime($fechaActual)) . ' de ' . $meses[$numeroMes - 1] . ' de ' . date('Y', strtotime($fechaActual));
+        echo '<p>' . $fechaCastellano . '</p>';
+    }
+    ?>
 
-?>
+</body>
+
+</html>
