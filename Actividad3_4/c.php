a1d2a88ad8899b837b79c1d765f997019cd52a22
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-03 12:26:56 +0200

    finishes c.php

--- Actividad3_4/c.php
+++ Actividad3_4/c.php
@@ -8,9 +8,21 @@
 </head>
 
 <body>
-    <?php
-    echo "me comes la verga";
-    ?>
+    <!-- para que la tabla se vaya autogenerando con el php se debe de meter el php dentro de la etiqueta tabla como en el ejemplo -->
+    <table align="left" border="1" cellpadding="3" cellspacing="2">
+        <tr>
+            <td>Nombre variable</td>
+            <td>Valor</td>
+        </tr>
+        <?php
+        foreach ($_SERVER as $conexion => $value) {
+            echo "<tr>";
+            echo "<td>$conexion</td>";
+            echo "<td>$value</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
 </body>
 
 </html>
