a72801e63c527780133d5843543b8e4fa68995a5
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-03 13:36:08 +0200

    small changes

--- Actividad3_4/e.php
+++ Actividad3_4/e.php
@@ -9,7 +9,7 @@
 
 <body>
     <?php
-    $paises = [
+    $mapa = [
         "España" => "Madrid",
         "Alemania" => "Berlin",
         "Italia" => "Roma",
@@ -20,13 +20,13 @@
 
 
 
-    print_r($paises);
+    print_r($mapa);
 
 
-    print "<hr>" . $paises["Italia"] . "<br>";
+    print "<hr>" . $mapa["Italia"] . "<br>";
 
 
-    if (key_exists("Rusia", $paises)) {
+    if (key_exists("Rusia", $mapa)) {
         echo "<hr>existe<br>";
     } else {
         echo "<hr>no existe<br>";
@@ -34,21 +34,21 @@
 
 
     echo "<hr>";
-    foreach ($paises as $pais => $ciudad) {
+    foreach ($mapa as $pais => $ciudad) {
         echo "$pais cuya capital es: $ciudad<br>";
     }
 
 
 
-    unset($paises["Francia"]);
+    unset($mapa["Francia"]);
 
-    echo "<hr> FRANCIA ELIMINADA, ARRIBA ESPAÑA<br>";
+    echo "<hr>FRANCIA ELIMINADA, ARRIBA ESPAÑA, EEEEEEL BICHO EL MEYOR JUGADOR DEL PLANETA TIERRA<br>";
 
-    foreach ($paises as $pais => $ciudad) {
+    foreach ($mapa as $pais => $ciudad) {
         echo "$pais cuya capital es: $ciudad<br>";
     }
 
-    echo "<hr>El país de Lisboa es: " . array_search("Lisboa", $paises);
+    echo "<hr>El país de Lisboa es: " . array_search("Lisboa", $mapa);
 
     ?>
 </body>
