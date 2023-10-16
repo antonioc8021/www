ed91f9157571f148737c1a5290ae169b276fdf0b
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-16 12:58:59 +0200

    no registra ni los piojos de un calvo

--- Actividad3_7/registro.php
+++ Actividad3_7/registro.php
@@ -4,33 +4,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejer C</title>
+    <title>Ejer que acabará conmigo</title>
 </head>
 
 <body>
-    <form action="c.php">
+    <h2><b>Registro de productos</b></h2>
+    <form action="registro.php" method="post">
+        Descripción: <input type="text" id="descripcion" name="descripcion" /><br><br>
 
+        Clave de categoría: <select name="claveCategoria" id="claveCategoria">
+            <option value="elegir"></option>
+            <?php
+            $codigo = ["INF", "JUV", "VET", "ALE", "CLASE_A", "CLASE_B", "CLASE_C"];
+            foreach ($codigo as $key) {
+                echo "<option value=$key>$key</option>";
+            }
+            ?>
+        </select> <br><br>
 
-        <select name="codigo" id="codigo">
-            <option value="ELIGE">Elige una opción</option>
-            <option value="INF">INF</option>
-            <option value="JUV">JUV</option>
-            <option value="VET">VET</option>
-            <option value="ALE">ALE</option>
-            <option value="CLASE_A">CLASE_A</option>
-            <option value="CLASE_B">CLASE_B</option>
-            <option value="CLASE_C">CLASE_C</option>
-        </select>
+        Precio <input type="number" id="precio" name="precio" /> €
         <br><br>
-        <input type="checkbox" id="importacion" name="Importación">Importacion
-        <input type="checkbox" id="diseno" name="Diseño">Diseño
-        <input type="checkbox" id="temporada" name="Temporada">Temporada
-        <input type="checkbox" id="piel " name="Piel">Piel
 
+        Stock <input type="number" id="stock" name="stock" /> Ud.
+        <br>
+
+        <h4>Recargos</h4>
+        <input type="checkbox" id="importacion" name="recargos">Importacion
+        <input type="checkbox" id="diseno" name="recargos">Diseño
+        <input type="checkbox" id="temporada" name="recargos">Temporada
+        <input type="checkbox" id="piel " name="recargos">Piel<br>
+        <input type="submit" id="enviar" value="enviar" />
     </form>
 
     <?php
-    $codigo = ["INF", "JUV", "VET", "ALE", "CLASE_A", "CLASE_B", "CLASE_C"];
+    if (isset($_POST['enviar'])) {
+        $descripcion = $_POST['descripcion'];
+        $claveCategoria = $_POST['claveCategoria'];
+        $precio = $_POST['precio'];
+        $stock = $_POST['stock'];
+        $recargos = $_POST['recargos'];
+        if (empty($descripcion) || empty($precio) || empty($stock)) {
+            echo ('Se deben de rellenar todos los campos');
+        } else {
+            echo ("ella perrea sola");
+        }
+    }
+
+
     ?>
 </body>
 
