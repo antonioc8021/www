421a5c1b05014866a8433ae063ed31762c61a344
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-17 12:46:37 +0200

    with a little bit of css

--- Actividad3_8/diccionario.php
+++ Actividad3_8/diccionario.php
@@ -5,9 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        h2.h2 {
+            text-align: center;
+        }
+
+        input[type=submit] {
+            margin: 17px;
+        }
+
+        input[type=text] {
+            margin-right: 50px;
+        }
+    </style>
 </head>
 
 <body>
+    <h2 class="h2">DICCIONARIO ESPAÑOL - INGLÉS</h2>
+
+    <form action="./diccionario.php" method="post" style="border: 1px solid; border-radius: 8px; padding: 20px">
+        Español: <input type="text" id="cajaEspanol" name="cajaEspanol">
+        Inglés: <input type="text" id="cajaEspanol" name="cajaEspanol">
+        <br> <br>
+        <input type="submit" name="enviar" value="Enviar">
+        <input type="submit" name="enviar" value="Eliminar">
+        <input type="submit" name="enviar" value="Buscar-Español">
+        <input type="submit" name="enviar" value="Buscar-Inglés">
+        <input type="submit" name="enviar" value="Listar">
+
+    </form>
+
 
 </body>
 
