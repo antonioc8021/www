ef060b1acb964bec042879782b981bdff8c2ece2
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-17 13:04:52 +0200

    make function añadir but don´t work

--- Actividad3_8/diccionario.php
+++ Actividad3_8/diccionario.php
@@ -25,16 +25,29 @@
 
     <form action="./diccionario.php" method="post" style="border: 1px solid; border-radius: 8px; padding: 20px">
         Español: <input type="text" id="cajaEspanol" name="cajaEspanol">
-        Inglés: <input type="text" id="cajaEspanol" name="cajaEspanol">
+        Inglés: <input type="text" id="cajaIngles" name="cajaIngles">
         <br> <br>
-        <input type="submit" name="enviar" value="Enviar">
-        <input type="submit" name="enviar" value="Eliminar">
-        <input type="submit" name="enviar" value="Buscar-Español">
-        <input type="submit" name="enviar" value="Buscar-Inglés">
-        <input type="submit" name="enviar" value="Listar">
-
+        <input type="submit" name="Anadir" value="Añadir">
+        <input type="submit" name="eliminar" value="Eliminar">
+        <input type="submit" name="buscarEspanol" value="Buscar-Español">
+        <input type="submit" name="buscarIngles" value="Buscar-Inglés">
+        <input type="submit" name="Listar" value="Listar">
     </form>
 
+    <?php
+    if (isset($_POST['anadir'])) {
+        if (empty($_POST['cajaEspanol']) && empty($_POST['cajaIngles'])) {
+            echo ("Para poder añadir debes de tener las dos cajas rellenadas");
+        } else {
+            $textoEspanol = array();
+            array_push($textoEspanol, $_POST['cajaEspanol']);
+            $textoIngles = $_POST['cajaIngles'];
+            foreach ($textoEspanol as $texto) {
+                return $texto;
+            }
+        }
+    }
+    ?>
 
 </body>
 
