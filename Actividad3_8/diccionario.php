7a573419df5bccbfc5449265770bc57d26acc774
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-17 14:02:08 +0200

    registro => anadir furula

--- Actividad3_8/diccionario.php
+++ Actividad3_8/diccionario.php
@@ -21,11 +21,33 @@
 </head>
 
 <body>
+    <?php
+    if (isset($_POST["diccionario"])) {
+        $diccionario = unserialize(base64_decode($_POST['diccionario']));
+    } else {
+        $diccionario = array();
+
+    }
+
+    if (isset($_POST['anadir'])) {
+        if (empty($_POST['cajaEspanol']) || empty($_POST['cajaIngles'])) {
+            echo ("Para poder añadir debes de tener las dos cajas rellenadas");
+        } else {
+            $palabraEspanol = $_POST['cajaEspanol'];
+            $palabraIngles = $_POST['cajaIngles'];
+            $diccionario[$palabraEspanol] = $palabraIngles;
+            print_r($diccionario);
+        }
+    }
+
+    // if (isset($_POST['listar']))
+    ?>
     <h2 class="h2">DICCIONARIO ESPAÑOL - INGLÉS</h2>
 
     <form action="./diccionario.php" method="post" style="border: 1px solid; border-radius: 8px; padding: 20px">
         Español: <input type="text" id="cajaEspanol" name="cajaEspanol">
         Inglés: <input type="text" id="cajaIngles" name="cajaIngles">
+        <input type="hidden" name="diccionario" value="<?php echo base64_encode(serialize($diccionario)); ?>">
         <br> <br>
         <input type="submit" name="anadir" value="Añadir">
         <input type="submit" name="eliminar" value="Eliminar">
@@ -33,24 +55,6 @@
         <input type="submit" name="buscarIngles" value="Buscar-Inglés">
         <input type="submit" name="Listar" value="Listar">
     </form>
-
-    <?php
-    $textoEspanol = array();
-    $textoIngles = array();
-    if (isset($_POST['anadir'])) {
-        if (empty($_POST['cajaEspanol']) || empty($_POST['cajaIngles'])) {
-            echo ("Para poder añadir debes de tener las dos cajas rellenadas");
-        } else {
-            $textoEspanol = $_POST['cajaEspanol'];
-            // array_push($textoEspanol, $_POST['cajaEspanol']);
-            array_push($textoIngles, $_POST['cajaIngles']);
-            print_r($textoEspanol);
-        }
-    }
-
-    // if (isset($_POST['listar']))
-    ?>
-
 </body>
 
 </html>
