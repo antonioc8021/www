33e254dd083ffbe54147c65a67523b51255e1653
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-23 11:58:43 +0200

    Create hotel.php

--- Tarea2/hotel.php
+++ Tarea2/hotel.php
@@ -4,10 +4,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Reserva Hotel</title>
+    <style>
+        .title {
+            text-align: center;
+        }
+
+        .form {
+            text-align: center;
+        }
+    </style>
+
 </head>
 
 <body>
+    <h1 class="title">RESERVA DE HABITACIONES DE HOTEL</h1>
+    <div class="form">
+        <form action="./hotel.php" method="post">
+
+
+        </form>
+        <fieldset>
+            <legend>HOTEL</legend>
+            Nombre Cliente
+            <input type="text" id="cliente" name="cliente"> <br>
+            Dias
+            <input type="number" id="dias" name="dias"> <br>
+
+        </fieldset>
+    </div>
 
 </body>
 
