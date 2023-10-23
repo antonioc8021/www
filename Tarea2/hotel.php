54ae686c9bd70209b755cb64788a88874d2a5739
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-10-23 12:44:42 +0200

    finishes the first div

--- Tarea2/hotel.php
+++ Tarea2/hotel.php
@@ -23,10 +23,28 @@
         <form action="./hotel.php" method="post">
             <fieldset>
                 <legend>HOTEL</legend>
-                Nombre Cliente
-                <input type="text" id="cliente" name="cliente"> <br>
+                <br>Nombre Cliente
+                <input type="text" id="cliente" name="cliente"> <br><br>
                 Dias
-                <input type="number" id="dias" name="dias"> <br>
+                <input type="number" id="dias" name="dias"> <br><br>
+                Tipo de habitación:
+                <select name="TipoHabitacion" id="TipoHabitacion">
+                    <option value="elegir"></option>
+                    <?php
+                    $tipoHabitacion = ["SUITE", "FAMILIAR", "TRIPLE", "DOBLE", "INDIVIDUAL"];
+                    foreach ($tipoHabitacion as $tipo) {
+                        echo "<option value=$tipo>$tipo</option>";
+                    }
+                    ?>
+                </select>
+                <p>Extras:
+                    <input type="checkbox" id="tenis" name="extras[]" value="tenis">Tenis</input>
+                    <input type="checkbox" id="masaje" name="extras[]" value="masaje">Masaje</input>
+                    <input type="checkbox" id="sauna" name="extras[]" value="sauna">Sauna</input>
+                    <input type="checkbox" id="peluquería" name="extras[]" value="peluqueria">Peluquería</input>
+                </p>
+                <input type="submit" id="limpiar" value="Limpiar" name="limpiar" />
+                <input type="submit" id="reservar" value="Reservar" name="reservar" />
             </fieldset>
         </form>
     </div>
