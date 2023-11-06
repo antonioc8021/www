f2a9d2884e41b918a9c284e090f78b070f7ffc0f
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-06 12:21:42 +0100

    Creates array telefonos

--- Examen1_Costas_Antonio/Ejercicio2/agenda.php
+++ Examen1_Costas_Antonio/Ejercicio2/agenda.php
@@ -9,7 +9,15 @@
 
 <body>
     <?php
-
+    $telefonos = array(
+        'Marta' => '675223344',
+        'Alberto' => '654321123',
+        'Luis' => '642335334',
+        'Kiko' => '632123261',
+        'Sandra' => '665667788',
+        'Rocío' => '689326598'
+    );
+    print_r($telefonos);
     ?>
 </body>
 
