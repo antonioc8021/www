82623ee025859e42851fe22869ca202bdf93c0d2
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-06 14:04:50 +0100

    finish perfectly bro

--- Examen1_Costas_Antonio/Ejercicio3/nomina.php
+++ Examen1_Costas_Antonio/Ejercicio3/nomina.php
@@ -23,10 +23,68 @@
                 <input type="number" id="sueldo" name="sueldo"><br>
                 <p>Extras:
                     <input type="checkbox" id="noche" name="extras[]" value="noche">Noche</input>
+                    <input type="checkbox" id="festivos" name="extras[]" value="festivos">Festivo</input>
+                    <input type="checkbox" id="horas extras" name="extras[]" value="horas extras">Horas</input>
+                    <input type="checkbox" id="peligro" name="extras[]" value="peligro">Peligro</input>
                 </p>
+                <p>Hijos:
+                    <input type="radio" name="hijos[]" id="uno" value="uno">1</input>
+                    <input type="radio" name="hijos[]" id="dos" value="dos">2</input>
+                    <input type="radio" name="hijos[]" id="tres" value="tres">3 o más</input>
+                </p>
+                <input type="reset" id="limpiar" , value="Limpiar" , name="limpiar" />
+                <input type="submit" id="calcular" , value="Calcular" , name="calcular" />
             </fieldset>
         </form>
     </div>
+
+    <?php
+    if (isset($_POST["calcular"])) {
+        if ((empty($_POST['sueldo'])) || (empty($_POST['extras'])) || (empty($_POST['hijos']))) {
+            echo 'Alguno de los elementos está vacío';
+        } else {
+            $tabla = array(
+                'noche' => 100,
+                'festivos' => 50,
+                'horas extras' => 50,
+                'peligro' => 150
+            );
+            $sueldo = $_POST['sueldo'];
+            $extras = [];
+            $extras = $_POST['extras'];
+            $hijos = [];
+            $hijos = $_POST['hijos'];
+        }
+
+        function calculaPrecio($sueldo, $extras, $hijos, $tabla)
+        {
+            $precioExtras = 0;
+            foreach ($extras as $extras) {
+                foreach ($tabla as $tablaExtra => $precio) {
+                    if ($extras == $tablaExtra) {
+                        $precioExtras += $precio;
+                    }
+                }
+            }
+            $precioHijos = 0;
+            foreach ($hijos as $hijos) {
+                if ($hijos == 'uno') {
+                    $precioHijos += 20;
+                } elseif ($hijos == 'dos') {
+                    $precioHijos += 40;
+                } elseif ($hijos == 'tres') {
+                    $precioHijos += 60;
+                } else {
+                    echo 'falla el precio';
+                }
+            }
+            echo ('Total a recibir: (' . $sueldo . '€ + ' . $precioExtras . '€ + ' . $precioHijos . '€) = ' . ($sueldo + $precioExtras + $precioHijos));
+
+        }
+        calculaPrecio($sueldo, $extras, $hijos, $tabla);
+    }
+
+    ?>
 
     <div id="pie">
     </div>
