eb8a9d22bf7e728d8b040c02384dfdf0ebefd924
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-06 12:45:32 +0100

    Eliminar works

--- Examen1_Costas_Antonio/Ejercicio2/agenda.php
+++ Examen1_Costas_Antonio/Ejercicio2/agenda.php
@@ -19,9 +19,18 @@
         'Rocío' => '689326598'
     );
 
+    print_r($telefonos);
+    echo '<br>';
+    echo '<br>';
     buscar('Luis', $telefonos);
     echo '<br>';
-    buscar('Rocío', $telefonos);
+    buscar('Emilio', $telefonos);
+    echo '<br>';
+    anadir('Antonio', '611111111', $telefonos);
+    echo '<br>';
+    anadir('Sandra', '622222222', $telefonos);
+    echo '<br>';
+    eliminar('Luis', $telefonos);
     echo '<br>';
     print_r($telefonos);
     ?>
--- Examen1_Costas_Antonio/Ejercicio2/funciones.php
+++ Examen1_Costas_Antonio/Ejercicio2/funciones.php
@@ -4,6 +4,29 @@ function buscar($nombre, $telefonos)
     if (array_key_exists($nombre, $telefonos)) {
         $numero = $telefonos[$nombre];
         echo "El número de: " . $nombre . ', es: ' . $numero;
+    } else {
+        echo 'No se encontró ningún contacto con el nombre introducido';
     }
 }
+
+function anadir($nombre, $numero, &$telefonos)
+{
+    if (!array_key_exists($nombre, $telefonos)) {
+        $telefonos[$nombre] = $numero;
+        echo 'Contacto registrado de manera correcta.';
+    } else {
+        echo 'Ya existe una persona con ese nombre.';
+    }
+}
+
+function eliminar($nombre, &$telefonos)
+{
+    if (array_key_exists($nombre, $telefonos)) {
+        unset($telefonos[$nombre]);
+        echo 'Se elimino el contacto de forma correcta';
+    } else {
+        echo 'No se encontró ningún contacto con el nombre introducido';
+    }
+}
+
 ?>
