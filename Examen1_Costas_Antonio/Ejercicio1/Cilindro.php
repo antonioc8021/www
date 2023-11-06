6f7ed4fb87d9f677be19d03e327919ed26e9b47b
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-06 12:07:22 +0100

    Ejer1 funciona

---
+++ Examen1_Costas_Antonio/Ejercicio1/Cilindro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    const PI = 3.14;
+
+    function volumen_cilindro($h, $r = 5)
+    {
+        $esPositivo = false;
+        if (($h >= 0) && ($r >= 0)) {
+            $esPositivo = true;
+        }
+
+        if ($esPositivo) {
+            return "Su volumen es: " . (PI * pow($r, 2) * $h);
+        } else {
+            return "Los valores introducidos tienen que ser positivos.";
+        }
+
+    }
+
+    echo volumen_cilindro(10, 2) . '<br>';
+    echo volumen_cilindro(3) . '<br>';
+    echo volumen_cilindro(-1) . '<br>';
+    ?>
+</body>
+
+</html>
