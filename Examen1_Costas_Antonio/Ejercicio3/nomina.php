95592f24e66050fdc40cd3a844445c5e79fd2e7c
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-06 13:11:20 +0100

    Creates file nomina

---
+++ Examen1_Costas_Antonio/Ejercicio3/nomina.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Examen DWES</title>
+    <link href="dwes.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <?php
+
+    ?>
+
+    <div id="encabezado">
+        <h1>Ejercicio 3: Nómina</h1>
+    </div>
+
+    <div id="contenido">
+
+    </div>
+
+    <div id="pie">
+    </div>
+
+</body>
+
+</html>
