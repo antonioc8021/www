0e75441d21c8b218beac677cd4316d6a002ed7cb
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-07 13:29:00 +0100

    creates listadoProductos.php

--- Tema4/conexion.php
+++ Tema4/conexion.php
@@ -1,6 +1,12 @@
 <?php
+$host = 'localhost';
+$user = 'super';
+$password = 'super123';
+$database = 'tienda';
+
 $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 $dwes = new PDO('mysql:host=localhost;dbname=tienda', 'super', 'super123', $opciones);
-$version = $dwes->getAttribute(PDO::ATTR_DRIVER_NAME);
-echo $version;
+$driverVersion = $dwes->getAttribute(PDO::ATTR_DRIVER_NAME);
+$serverVersion = $dwes->getAttribute(PDO::ATTR_SERVER_VERSION);
+
 ?>
--- Tema4/listadoClientes.php
+++ Tema4/listadoClientes.php
@@ -1,4 +1,10 @@
 <?php
 include_once("conexion.php");
+$sql = "SELECT * FROM cliente ORDER BY nif limit 10";
+$result = $dwes->query($sql);
+while ($row = $result->fetch()) {
+    echo $row['nombre'] . ' - ' . $row['direccion'] . ' - ' . $row['telefono'] . '<br>';
+}
 
+unset($dwes);
 ?>
