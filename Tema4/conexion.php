58cc46b8bb2664e400f0624ad60ee92d6a73e9a8
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-07 13:05:59 +0100

    add utf8

--- Tema4/conexion.php
+++ Tema4/conexion.php
@@ -1,5 +1,6 @@
 <?php
-$dwes = new PDO('mysql:host=localhost;dbname=tienda', 'super', 'super123');
+$opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+$dwes = new PDO('mysql:host=localhost;dbname=tienda', 'super', 'super123', $opciones);
 $version = $dwes->getAttribute(PDO::ATTR_DRIVER_NAME);
 echo $version;
 ?>
