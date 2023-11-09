7cd57a5da220e64730714d38505ef063b2da56a5
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-09 11:42:51 +0100

    Import exercises from the teacher

--- Tema4/listadoProductos.php
+++ Tema4/listadoProductos.php
@@ -1,3 +1,8 @@
 <?php
 include_once("conexion.php");
+$sql = "SELECT nombre, precio, descripcion from producto order by nombre";
+$result = $dwes->query($sql);
+while ($row = $result->fetch()) {
+    echo $row["nombre"] . " - " . $row["precio"] . " - " . $row["descripcion"] . '<br>';
+}
 ?>
