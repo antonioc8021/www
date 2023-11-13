ec45230567e1e7e22f5017575b8b9c70281b44f5
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-13 13:20:21 +0100

    let´s to do it

---
+++ Tema4/Actividad4_3/conexion.php
@@ -0,0 +1,20 @@
+<!-- Modo de conexion haciendo un try catch para capturar la excepciones -->
+<?php
+$host = 'localhost';
+$user = 'goyas';
+$password = 'goyas123';
+$database = 'goyas';
+
+try {
+    $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+    // muy importante, realizar la instrucción de abajo con comillas dobles y no simples.
+    $conn = new PDO("mysql:host=localhost;dbname=$database", $user, $password, $opciones);
+} catch (PDOException $e) {
+    echo $e->getCode();
+    $mensaje = $e->getMessage();
+    echo 'Error en la conexión' . $mensaje;
+    exit();
+}
+
+
+?>
---
+++ Tema4/Actividad4_3/familias.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Familias</title>
+</head>
+
+<body>
+    <div id="cabeza">
+        <h2>EJERCICIO: OBTENCIÓN DE PRODUCTOS DE UNA FAMILIA</h2>
+        <p>Familia: <select name="familia" id="fam">
+                <?php
+                
+                    $familias=
+                ?>
+            </select></p>
+    </div>
+
+
+    <?php
+
+
+
+    ?>
+
+</body>
+
+</html>
