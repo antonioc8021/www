28ce038d0e6c8949fa65fc0de07189b0b9a5c680
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2023-11-13 14:02:41 +0100

    todo: make the conexion with sql

--- Tema4/Actividad4_3/familias.php
+++ Tema4/Actividad4_3/familias.php
@@ -5,17 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Familias</title>
+    <link href="dwes.css" rel="stylesheet" type="text/css">
+
 </head>
 
 <body>
-    <div id="cabeza">
-        <h2>EJERCICIO: OBTENCIÓN DE PRODUCTOS DE UNA FAMILIA</h2>
+    <div id="encabezado">
+        <h1>Ejercicio: Obtención de Productos de una Familia</h1>
         <form id="form_action" action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
             <span>Familia: </span>
             <select name="familia" id="familia">
                 <?php
                 if (isset($_POST['familia'])) {
                     $familia = $_POST['familia'];
+
                 }
                 $dwes = new PDO("mysql:host=localhost;dbname=dwes", "dwes", "abc123.");
                 $sqlº
