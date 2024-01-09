a9242259b7fffc70828271b015d4989c0e74bd2e
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-01-09 12:20:39 +0100

    creates theme 6

---
+++ Tema6/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Plantilla</title>
+
+</head>
+<body>
+    <?php
+        require_once('coche.php');
+        $coche = new Coche(20,"8281-GML");
+        echo $coche->mostrar();
+    
+    ?>
+</body>
+</html>
