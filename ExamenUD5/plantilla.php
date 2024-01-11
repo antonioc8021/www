b49d8a2ab7492c614c7cf732df4b4e2afb9fe7ed
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-01-11 11:41:11 +0100

    create folder for exam

---
+++ ExamenUD5/plantilla.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Examen DWES</title>
+  <link rel="stylesheet" href="dwes.css">
+</head>
+
+<body>
+
+  <div id="encabezado">
+    <h1>Ejercicio: </h1>
+  </div>
+
+  <div id="contenido">
+    <h2>Agenda de correos electrónicos</h2>
+    <form action="" method="post">
+      <p>Titular: <input type="text" name="titular" id="titular"></p>
+      <p>Email: <input type="email" name="titular" id="titular"></p>
+      <p>
+        <input type="submit" value="Añadir" name="anadir">
+        <input type="reset" value="Eliminar" name="eliminar">
+        <input type="submit" value="Buscar" name="buscar">
+        <input type="submit" value="Listado" name="listado">
+        <input type="submit" value="CERRAR" name="cerrar">
+
+      </p>
+    </form>
+  </div>
+
+  <div id="pie">
+  </div>
+
+</body>
+
+</html>
