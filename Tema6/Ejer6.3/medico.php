4192efb273fc77f589052126f75e5530a431a613
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-01-16 12:14:43 +0100

    renames

--- Tema6/Ejer6.3/medico.php
+++ Tema6/Ejer6.3/medico.php
@@ -1,2 +1,72 @@
 <?php
 
+abstract class Medico
+{
+    protected $nombre;
+    protected $edad;
+    protected $turno;
+
+    public
+
+
+
+        /**
+         * Get the value of nombre
+         */
+        public function getNombre(
+    ) {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of edad
+     */
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+
+    /**
+     * Set the value of edad
+     *
+     * @return  self
+     */
+    public function setEdad($edad)
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of turno
+     */
+    public function getTurno()
+    {
+        return $this->turno;
+    }
+
+    /**
+     * Set the value of turno
+     *
+     * @return  self
+     */
+    public function setTurno($turno)
+    {
+        $this->turno = $turno;
+
+        return $this;
+    }
+}
