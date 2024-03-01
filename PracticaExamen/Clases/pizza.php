03cddc458963f8c4ad0e54ebfd7a3fb78c102e76
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-03-01 19:15:50 +0100

    finishes pizza.php

--- PracticaExamen/Clases/pizza.php
+++ PracticaExamen/Clases/pizza.php
@@ -8,7 +8,63 @@ class Pizza
     protected $tipo;
     protected $foto;
 
+    // creo constructor
+    function __construct($codigo, $descripcion, $precio, $tipo, $foto)
+    {
+        $this->codigo = $codigo;
+        $this->descripcion = $descripcion;
+        $this->precio = $precio;
+        $this->tipo = $tipo;
+        $this->foto = $foto;
+    }
 
+    function getCodigo()
+    {
+        return $this->codigo;
+    }
 
+    function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+    }
 
+    function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+    }
+
+    function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    function setPrecio($precio)
+    {
+        $this->precio = $precio;
+    }
+
+    function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+
+    function getFoto()
+    {
+        return $this->foto;
+    }
+
+    function setFoto($foto)
+    {
+        $this->foto = $foto;
+    }
 }
