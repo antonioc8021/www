1d2c1bb5394a15de533e37a90d6cdc7fd4b0d49b
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-03-01 18:57:09 +0100

    Movin the project to the main branch of the project

---
+++ PracticaExamen/Clases/pizza.php
@@ -0,0 +1,14 @@
+<?php
+class Pizza
+{
+    // atributos de la clase
+    protected $codigo;
+    protected $descripcion;
+    protected $precio;
+    protected $tipo;
+    protected $foto;
+
+
+
+
+}
